b37e1b54f1290863f88673d7ba4b50f5fb873916
odinN <odinntap@gmail.com>
2023-09-06 15:14:33 +0200

    seo optimization

--- packages/frontend/src/locale/translations.ts
+++ packages/frontend/src/locale/translations.ts
@@ -1,4 +1,4 @@
 export const translations = {
-  title: $localize`:@@title:Православные Святыни Юга Франции`,
+  title: $localize`:@@title:Православные Святыни Юга Франции: Откройте Духовное Наследие`,
   searchPlaceholder: $localize`:@@search_placeholder:Имя, город, почтовый индекс...`
 }
