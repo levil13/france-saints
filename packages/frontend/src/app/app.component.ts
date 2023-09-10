23963baf8fed2b420a6037ddf46509fe3ed28e38
Ilia Mishchenko <ilia.mishchenko@eltex.loc>
2023-09-10 17:58:30 +0700

    Phone screen fix

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.updateVh();
+    requestAnimationFrame(() => this.updateVh())
   }
 
   private updateVh() {
